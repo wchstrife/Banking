ecb23b8ab9f60098b12fcdec7a39ae3e79f95855
Administrator <Administrator@R320PC28>
2017-07-05 15:22:26 +0800

    bank

--- src/bank/Bank.java
+++ src/bank/Bank.java
@@ -1,15 +1,32 @@
 package bank;
 
+import javax.print.attribute.standard.NumberOfDocuments;
+
 public class Bank {
 	
 	Customer[] customers;
 	
-	int numberOfCustomers;
+	int numberOfCustomers = 0;
 
 	public Bank() {
 		customers = new Customer[5];
 	}
 	
+	public void addCustomer(String firstName, String lastName){
+		Customer co = new Customer(firstName, lastName);
+		customers[numberOfCustomers] = co;
+		numberOfCustomers++;
+	}
+
+	public int getNumberOfCustomers() {
+		return numberOfCustomers;
+	}
+
+	public Customer getCustomers(int index) {
+		return customers[index];
+	}
+
 	
+
 	
 }
